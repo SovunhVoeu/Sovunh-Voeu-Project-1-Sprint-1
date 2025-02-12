febebf9ca3aa369712f56bbd8fe31102b2dc3e2b
Sovunh Voeu <svoeu@student.bridgwe.edu>
2025-02-11 23:18:13 -0500

    fix indentation per .yml request

--- tests/test_data.py
+++ tests/test_data.py
@@ -55,14 +55,14 @@ def test_json_file_rapidJobs():
     test_file = 'test_data2.json'
     test_data = [[{"id": "f97b4a007d08a432", "site": "indeed",
                   "job_url": "https://www.indeed.com/viewjob?jk=f97b4a007d08a432",
-                  "job_url_direct": "http://www.indeed.com/job/information"
+                   "job_url_direct": "http://www.indeed.com/job/information"
                                     "-security-analystsr-information-security-analyst-f97b4a007d08a432"}],
                  [{"id": "9d51f80c2334c33f", "site": "indeed",
                   "job_url": "https://www.indeed.com/viewjob?jk=9d51f80c2334c33f",
-                  "job_url_direct": "http://www.indeed.com/job/help-desk-production-support-9d51f80c2334c33f"}],
+                   "job_url_direct": "http://www.indeed.com/job/help-desk-production-support-9d51f80c2334c33f"}],
                  [{"id": "cee95e7fa46f8677", "site": "indeed",
                   "job_url": "https://www.indeed.com/viewjob?jk=cee95e7fa46f8677",
-                  "job_url_direct": "https://careersatricoh.com/FO/P6IFK026203F3VBQB688NF6WN/"
+                   "job_url_direct": "https://careersatricoh.com/FO/P6IFK026203F3VBQB688NF6WN/"
                                     "components/details.html?jobId=16277&jobTitle=Customer%20Support%20Technician"}]]
     with open(test_file, "w") as file:
         for item in test_data:
