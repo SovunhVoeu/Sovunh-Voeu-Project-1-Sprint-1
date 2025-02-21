d5c42cad1ec56dd31f84fbd9adf9f8f4ecf084fa
Sovunh Voeu <svoeu@student.bridgwe.edu>
2025-02-20 22:18:25 -0500

    Fix the error with the Jobs details not showing. Also added loops to iterate

--- gui.py
+++ gui.py
@@ -2,10 +2,12 @@
 Author: Sovunh Voeu
 Date: 2/18/2025
 """
+import sqlite3
 import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QVBoxLayout, QWidget, QTableView, QPushButton,
-                             QLineEdit, QFormLayout, QHBoxLayout, QMessageBox, QLabel)
-from PyQt6.QtSql import QSqlDatabase, QSqlTableModel, QSqlQueryModel
+                             QLineEdit, QFormLayout, QHBoxLayout, QMessageBox, QLabel, QTextEdit)
+from PyQt6.QtSql import QSqlDatabase, QSqlTableModel, QSqlQueryModel, QSqlQuery
+from PyQt6.QtCore import Qt
 
 
 class SecondWindow(QWidget):
@@ -46,7 +48,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Database Viewer Test")
+        self.setWindowTitle("Database Viewer")
         self.setGeometry(100, 100, 800, 600)
 
         central_widget = QWidget()
@@ -56,25 +58,32 @@ class MainWindow(QMainWindow):
         self.db = QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('jobs.db')
         if not self.db.open():
-            print("Unable to open database")
+            QMessageBox.critical(self, "Database Error", "Unable to open database")
             return
 
+        self.details_text = QTextEdit()
+        self.details_text.setReadOnly(True)
+        self.details_text.setLineWrapMode(QTextEdit.LineWrapMode.WidgetWidth)
+        layout.addWidget(self.details_text)
+
         self.table_view1 = QTableView()
-        self.model1 = QSqlTableModel()
-        self.model1.setTable('rapidResults')
-        self.model1.select()
+        self.model1 = QSqlQueryModel()
+        self.model1.setQuery("SELECT * FROM rapidResults", self.db)
         self.table_view1.setModel(self.model1)
-        self.table_view1.clicked.connect(self.display_details)
+        for col in range(self.model1.columnCount()):
+            if col != 4:
+                self.table_view1.setColumnHidden(col, True)
+        self.table_view1.clicked.connect(self.display_details1)
         layout.addWidget(self.table_view1)
 
-        self.details_label = QLabel("Select a Job so that you may see the full details.")
-        layout.addWidget(self.details_label)
-
         self.table_view2 = QTableView()
-        self.model2 = QSqlTableModel()
-        self.model2.setTable('rapid_jobs2')
-        self.model2.select()
+        self.model2 = QSqlQueryModel()
+        self.model2.setQuery("SELECT * FROM rapid_jobs2", self.db)
         self.table_view2.setModel(self.model2)
+        for col in range(self.model2.columnCount()):
+            if col != 1:
+                self.table_view2.setColumnHidden(col, True)
+        self.table_view2.clicked.connect(self.display_details2)
         layout.addWidget(self.table_view2)
 
         button = QPushButton("Open Search Window")
@@ -83,22 +92,38 @@ class MainWindow(QMainWindow):
 
         self.second_window = None
 
+
     def open_second_window(self):
         if self.second_window is None:
             self.second_window = SecondWindow(self)
         self.second_window.show()
 
-    def search_table1(self, search_term):
-        query = f"site LIKE '%{search_term}%'"
-        self.model1.setFilter(query)
-        self.model1.select()
 
-    def display_details(self, index):
+    # def search_table1(self, search_term):
+    #     query = f"site LIKE '%{search_term}%'"
+    #     self.model1.setFilter(query)
+    #     self.model1.select()
+
+
+    def display_details1(self, index):
         row = index.row()
-        job_id = self.model1.data(self.model1.index(row, 0))
-        job_name = self.model1.data(self.model1.index(row, 4))
-        job_details = f"ID: {job_id}\nTitle: {job_name}"
-        self.details_label.setText(job_details)
+        job_details = []
+        for col in range(self.model1.columnCount()):
+            column_name = self.model1.headerData(col, Qt.Orientation.Horizontal)
+            column_value = self.model1.data(self.model1.index(row, col))
+            job_details.append(f"{column_name}: {column_value}")
+        self.details_text.setText("\n".join(job_details))
+
+
+    def display_details2(self, index):
+        row = index.row()
+        job_details = []
+        for col in range(self.model2.columnCount()):
+            column_name = self.model2.headerData(col, Qt.Orientation.Horizontal)
+            column_value = self.model2.data(self.model2.index(row, col))
+            job_details.append(f"{column_name}: {column_value}")
+        self.details_text.setText("\n".join(job_details))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
